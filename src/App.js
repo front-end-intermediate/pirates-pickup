8fab43858a5fc65d13b600f0e567312be046fcf9
DannyBoyNYC <daniel.deverell@gmail.com>
2024-03-29 15:08:03 -0400

    done

--- src/App.js
+++ src/App.js
@@ -25,12 +25,24 @@ function App() {
 
   const addPirate = (pirate) => {
     pirate.image = "avatar.png";
-    setPirates((prev) => [pirate, ...prev]);
+    fetch("http://localhost:3001/pirates", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pirate),
+    })
+      .then((res) => res.json())
+      .then((newPirate) => setPirates([newPirate, ...pirates]));
   };
 
-  const removePirate = (name) => {
-    const newPirates = pirates.filter((pirate) => pirate.name !== name);
-    setPirates(newPirates);
+  const removePirate = (pirateId) => {
+    fetch(`http://localhost:3001/pirates/${pirateId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((pirate) => console.log(pirate))
+      .then(setPirates(pirates.filter((pirate) => pirate.id !== pirateId)));
   };
 
   return (
